dcf712c3d6f20b0c8a7055ac9a553f3e610be95a
KirtTower <115346343+KirtTower@users.noreply.github.com>
2022-10-15 12:28:38 +0300

    Delete checkTemp.js

--- checkTemp.js
+++
@@ -1,8 +0,0 @@
-function checkTemp(temp) {
-    if (temp >= 0 && temp <= 20)
-        console.log("-1")
-    else if (temp >= 21 && temp <= 40)
-        console.log("0")
-    else if (temp >= 41 && temp <= 60)
-        console.log("1")
-}
