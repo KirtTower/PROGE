d4381ca09c7f828fcb2bfac14b94764a831e7657
KirtTower <115346343+KirtTower@users.noreply.github.com>
2022-10-15 13:43:43 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,8 @@
+function checkTemp(temp) {
+    if (temp >= 0 && temp <= 20)
+        return -1
+    if (temp >= 21 && temp <= 40)
+        return 0
+    if (temp >= 41 && temp <= 60)
+        return 1
+}
